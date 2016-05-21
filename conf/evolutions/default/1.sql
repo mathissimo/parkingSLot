4559b7422134f2ee25e94872c251cea56b908a70
Mathias Münscher <mmsec@t101.de>
2016-05-21 14:41:51 +0200

    Add create table

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,6 +1,17 @@
 # --- !Ups
 
-create table "suppliers" ("id" BIGSERIAL NOT NULL PRIMARY KEY,"name" VARCHAR(254) NOT NULL,"desc" VARCHAR(254) NOT NULL);
+-- create table "suppliers" ("id" BIGSERIAL NOT NULL PRIMARY KEY,"name" VARCHAR(254) NOT NULL,"desc" VARCHAR(254) NOT NULL);
+
+Christoph Knabe (knabe@beuth-hochschule.de)
+create table "users" (
+  "id" BIGSERIAL NOT NULL PRIMARY KEY,
+  "login" VARCHAR(254) NOT NULL UNIQUE --unique username
+);
+
+create table "users" (
+  "id" BIGSERIAL NOT NULL PRIMARY KEY,
+  "login" VARCHAR(254) NOT NULL UNIQUE --unique username
+);
 
 # --- !Downs
 ;
