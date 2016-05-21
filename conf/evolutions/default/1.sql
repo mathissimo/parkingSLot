fe84da29f5b5332691b493a0525008b1833753c9
Christoph Knabe <knabe@beuth-hochschule.de>
2016-05-21 14:50:42 +0200

    Create tables

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,17 +2,18 @@
 
 -- create table "suppliers" ("id" BIGSERIAL NOT NULL PRIMARY KEY,"name" VARCHAR(254) NOT NULL,"desc" VARCHAR(254) NOT NULL);
 
-Christoph Knabe (knabe@beuth-hochschule.de)
 create table "users" (
   "id" BIGSERIAL NOT NULL PRIMARY KEY,
   "login" VARCHAR(254) NOT NULL UNIQUE --unique username
 );
 
-create table "users" (
+create table "parking_slots" (
   "id" BIGSERIAL NOT NULL PRIMARY KEY,
-  "login" VARCHAR(254) NOT NULL UNIQUE --unique username
+  "lat" DOUBLE NOT NULL,
+  "lon" DOUBLE NOT NULL,
+  "free_from" TIMESTAMP
 );
 
 # --- !Downs
 ;
-drop table "suppliers";
+drop table "users", "parking_slots";
